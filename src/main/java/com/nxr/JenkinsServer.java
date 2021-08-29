01125ebfccb2391df03fa812d98d9bee05dc0a00
xiangru.ni <3393982576@qq.com>
2021-08-29 14:08:36 +0800

    初始化

---
+++ src/main/java/com/nxr/JenkinsServer.java
@@ -0,0 +1,11 @@
+package com.nxr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsServer {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsServer.class,args);
+    }
+}
