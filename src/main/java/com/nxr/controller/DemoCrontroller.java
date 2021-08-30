198da0ca0efb55cbe59cc3c3853e677341b196ea
xiangru.ni <3393982576@qq.com>
2021-08-30 21:49:35 +0800

    提交一下2

--- src/main/java/com/nxr/controller/DemoCrontroller.java
+++ src/main/java/com/nxr/controller/DemoCrontroller.java
@@ -1,10 +1,12 @@
 package com.nxr.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoCrontroller {
 
+    @RequestMapping
     String getName() {
         return "";
     }
