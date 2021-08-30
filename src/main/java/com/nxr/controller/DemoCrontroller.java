4e729b05b16c037c04d5e193655a0ee1260c5f3e
xiangru.ni <3393982576@qq.com>
2021-08-30 20:40:57 +0800

    提交一下

--- src/main/java/com/nxr/controller/DemoCrontroller.java
+++ src/main/java/com/nxr/controller/DemoCrontroller.java
@@ -1,4 +1,11 @@
 package com.nxr.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoCrontroller {
+
+    String getName() {
+        return "";
+    }
 }
